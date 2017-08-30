b72b029c27d2df001aa3a02637b37d32b8803df2
Sébastien Becquerel <s.becquerel@epiconcept.fr>
2017-08-30 23:28:51 +0200

    Use middleware to catch errors (+ wrapAsync)

--- app.js
+++ app.js
@@ -10,85 +10,72 @@ const db = require('./services/databaseService.js')(config.get('dbConfig'));
 const auth = require('./services/authService.js')(db);
 const videoService = require('./services/videoService.js');
 
+function wrapAsync(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
 app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({
     extended: true
   }))
-  .post('/auth', (req, res) => {
+  .post('/auth', async (req, res) => {
     if (req.body.username === undefined || req.body.password === undefined) {
+      console.log('auth');
+      
       return res.status(500).end();
     }
     const username = req.body.username;
     const password = req.body.password;
 
-    auth.authentify(username, password)
-      .then(token => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({username, token}));
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(403).end()
-      });
+    try {
+      const token = await auth.authentify(username, password);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({username, token}));
+    } catch (err) {
+      console.log(err);
+      res.status(403).end()
+    }
   })
-  .get('/video/:videoId/t/:token', (req, res) => {
+  .get('/video/:videoId/t/:token', wrapAsync(async (req, res) => {
     const token = req.params.token;
 
     if (req.params.videoId === undefined || req.params.token === undefined) {
       return res.status(500).end();
     }
 
-    auth.validToken(token)
-      .then(groupId => {
-        const videoId = req.params.videoId;
-        const video = new videoService(db);
+    const groupId = await auth.validToken(token);
+    const videoId = req.params.videoId;
+    const video = new videoService(db);
         
-        video.setPlayed(videoId, groupId)
-          .then(() => video.getVideoPath(videoId))
-          .then(videoPath => video.stream(req, res, videoPath))
-          /*.then(videoPath => {
-            res.setHeader('Content-Type', 'application/json');
-            res.send({res: 'ok'});
-          })*/
-          .catch(err => {
-            console.log('Error: ', err);
-            res.status(500).end();
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(403).end()
-      });
-  })    
-  .get('/video/:videoId/thumbnail/:size/:index?', (req, res) => {
+    await video.setPlayed(videoId, groupId);
+
+    const videoPath = await video.getVideoPath(videoId);
+
+    await video.stream(req, res, videoPath);
+  }))
+  .get('/video/:videoId/thumbnail/:size/:index?', wrapAsync(async (req, res) => {
     const video = new videoService(db);
     const videoId = req.params.videoId;
     const thumbnailIndex = req.params.index;
     const thumbnailSize = req.params.size;
 
-    const videoPath = video.getVideoPath(videoId)
-      .then(videoPath => {
-        let file;
-        
-        if ( thumbnailIndex !== undefined ) {
-          file = `${videoPath}-${thumbnailSize}-${thumbnailIndex}.png`;
-        } else {
-          file = `${videoPath}-${thumbnailSize}.png`;
-        }
+    const videoPath = await video.getVideoPath(videoId);
+    let file;
+    
+    if ( thumbnailIndex !== undefined ) {
+      file = `${videoPath}-${thumbnailSize}-${thumbnailIndex}.png`;
+    } else {
+      file = `${videoPath}-${thumbnailSize}.png`;
+    }
 
-        if ( ! fs.existsSync(file)) {
-          return res.status(500).end();
-        }
-        res.setHeader('Content-Type', 'image/png');
-        res.send(fs.readFileSync(file));
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).end();
-      })
-  })    
-  .use((req, res, next) => {
+    if ( ! fs.existsSync(file)) {
+      return res.status(500).end();
+    }
+    res.setHeader('Content-Type', 'image/png');
+    res.send(fs.readFileSync(file));
+  }))    
+  .use(async (req, res, next) => {
     const authorization = req.header('Authorization');
     if (authorization === undefined) {
       return res.status(403).end();
@@ -101,40 +88,30 @@ app
 
     const token = matchRes[1];
 
-    auth.validToken(token)
-      .then(groupId => {
-        res.locals.token = token;
-        res.locals.groupId = groupId;        
-        next();
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(403).end()
-      });
+    try {
+      const groupId = await auth.validToken(token);
+      res.locals.token = token;
+      res.locals.groupId = groupId;        
+      next();
+    } catch(err) {
+      console.log(err);
+      res.status(403).end()
+    }
   })
-  .get('/logout', (req, res) => {
-    auth.rmToken(res.locals.token)
-      .then(() => res.status(200).end())
-      .catch(err => {
-        console.log(err);
-        res.status(500).end();
-      })
-  })
-  .get('/video', (req, res) => {
+  .get('/logout', wrapAsync(async (req, res) => {
+    await auth.rmToken(res.locals.token);
+    res.status(200).end();
+  }))
+  // GET video list
+  .get('/video', wrapAsync(async (req, res) => {
     const video = new videoService(db);
     const groupId = res.locals.groupId;
 
-    video.list(groupId)
-      .then(videoList => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(videoList));
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(403).end();
-      });
-  })  
-  .post('/video/:videoId', (req, res) => {
+    const videoList = await video.list(groupId);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(videoList));
+  }))
+  .post('/video/:videoId', wrapAsync(async (req, res) => {
     if (req.body.favorite === undefined) {
       return res.status(500).end();
     }
@@ -148,13 +125,13 @@ app
     const groupId = res.locals.groupId;
     const videoId = req.params.videoId;    
 
-    video.setFavorite(videoId, groupId, favorite)
-      .then(() => res.status(200).end())
-      .catch(err => {
-        console.log(err);
-        res.status(500).end();
-      });
-  })  
+    await video.setFavorite(videoId, groupId, favorite);
+    res.status(200).end();
+  })) 
+  .use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).end();
+  }) 
   .use((req, res, next) => {
     res.status(404).end();
   })
